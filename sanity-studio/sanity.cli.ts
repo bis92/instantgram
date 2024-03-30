a3ae2f3297572988756a005784b08460477fb579
Bis92 <bis920309@gmail.com>
2024-03-30 23:52:03 +0900

    set up project

---
+++ sanity-studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '96qu6goc',
+    dataset: 'production'
+  }
+})
---
+++ src/components/ui/icons/SearchFillIcon.tsx
@@ -0,0 +1,6 @@
+import { RiSearchFill } from 'react-icons/ri';
+
+export default function SearchFillIcon() {
+  return <RiSearchFill className='w-7 h-7' />;
+}
+
