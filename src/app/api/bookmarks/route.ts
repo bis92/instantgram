cc181382f30757ce85f4a40479b0fe52f5ab83b8
Bis92 <bis920309@gmail.com>
2024-06-22 23:07:09 +0900

    Refactor: get session auth

--- src/app/api/bookmarks/route.ts
+++ src/app/api/bookmarks/route.ts
@@ -1,24 +1,16 @@
-import { authOptions } from '@/app/lib/auth';
+import { withSessionUser } from '@/util/session';
 import { addBookmark, removeBookmark } from '@/service/user';
-import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function PUT(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-
-  if(!user) {
-    return new Response('Authentication Error', { status: 401 });
-  }
-
-  const { id, bookmark } = await req.json();
-
-  if(!id || bookmark === undefined) {
-    return new Response('Bad Request', { status: 400 })
-  }
-
-  const request = bookmark ? addBookmark : removeBookmark;
-  return request(user.id, id)
-    .then(res => NextResponse.json(res))
-    .catch(error => new Response(JSON.stringify(error), { status: 500 }))
+  return withSessionUser(async (user) => {
+    const { id, bookmark } = await req.json();
+    if(!id || bookmark == null) {
+      return new Response('Bad Request', { status: 400 })
+    }
+    const request = bookmark ? addBookmark : removeBookmark;
+    return request(user.id, id)
+      .then(res => NextResponse.json(res))
+      .catch(error => new Response(JSON.stringify(error), { status: 500 }))
+  })
 }
--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,15 +1,10 @@
-import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 import { getUserByUsername } from '@/service/user';
-import { authOptions } from '@/app/lib/auth';
+import { withSessionUser } from '@/util/session';
 
 export async function GET() {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-  if(!user) {
-    return new Response('Authentication Error', { status: 401 });
-  }
-
-  return getUserByUsername(user.username)
-  .then(data => NextResponse.json(data));
+  return withSessionUser(async (user) => 
+    getUserByUsername(user.username)
+    .then(data => NextResponse.json(data))
+  )
 }
